0915e15ff565508505022215f7137a00f1b7cda1
Estibaliz Lorenzo <esti_2612@hotmail.com>
2023-05-26 17:06:44 +0200

    adds photos code in the visual code

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   validates :name, uniqueness: true, presence: true, allow_blank: false, allow_nil: false
+  has_one_attached :photo
 end
